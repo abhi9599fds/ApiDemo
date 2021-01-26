21f8b7dc8e0296b73419e2721cfc192dd1920f70
abhi9599fds <abhishekprasad9599@gmail.com>
2021-01-26 21:10:40 +0530

    make it better

--- data/dataConnection.js
+++ data/dataConnection.js
@@ -1,15 +1,24 @@
 import config  from "dotenv";
-
+//const st = "postgres://tfgytbfruesaxj:f2677809ba562d6ee197aa2332a415459864668cb2c67976eaa8aa2532bb0465@ec2-3-231-241-17.compute-1.amazonaws.com:5432/d29pfls8jaafg2"
 config();
 
-process.env.DATABASE_URL
+var st = process.env.DATABASE_URL;
+var connUrl = st.replace("postgres://","");
+var pgUserPass = connUrl.split('@')[0]
+var pgHostPortDb = connUrl.split("@")[1];
+var pgHostPort = pgHostPortDb.split("/")[0];
+var pgDb = pgHostPortDb.split("/")[1];
+var pgUser = pgUserPass.split(":")[0];
+var pgPass = pgUserPass.split(":")[1];
+var pgHost = pgHostPort.split(":")[0];
+var pgPort = pgHostPort.split(":")[1];
 
 const dbConn =  {
-    HOST : "localhost",
-    USER : "abhi9599",
-    PASSWORD : "1234",
-    PORT : "5432",
-    DATABASE : "mvpdatabase",
+    HOST : pgHost,
+    USER : pgUser,
+    PASSWORD : pgPass,
+    PORT : pgPort,
+    DATABASE : pgDb,
     dialect: "postgres",
     pool :{
         max: 5,
